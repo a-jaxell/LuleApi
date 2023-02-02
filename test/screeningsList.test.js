d20b1fda95967301c6278415d75d9b5e15f1b455
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-02-02 23:12:25 +0100

    Added import of separated apiHandler

--- test/screeningsList.test.js
+++ test/screeningsList.test.js
@@ -1,11 +1,13 @@
-import { getScreeningsList } from '../server/screeningsList.js';
+import { getScreeningsList, getScreeningsWithMovies } from '../server/screeningsList.js';
 import { mockApiAdapter } from './mockApiAdapter.js'
 
 
 describe('Control if data recieved has correct format and size', () => {
 
+    
+
     test('Filters out screenings that are more than 5 days ahead', async () => {
-        const data = await getScreeningsList();
+        const data = await getScreeningsList(getScreeningsWithMovies);
         const date = new Date();
         
         expect(data.every(ele => {
@@ -14,16 +16,16 @@ describe('Control if data recieved has correct format and size', () => {
   
     });
     test('Date and time is of correct format', async () =>{
-        const data = await getScreeningsList();        
+        const data = await getScreeningsList(getScreeningsWithMovies);        
         
-        expect(Date.parse(`${data[4].start_time}`)).toBeTruthy();
         expect(data[5].start_time).not.toBeUndefined();
+        expect(Date.parse(`${data[4].start_time}`)).toBeTruthy();
         
     })
     test('Data is an array and does not contain more than 10 entities of data', async () => {
-        const data = await getScreeningsList();
+        const data = await getScreeningsList(getScreeningsWithMovies);
         
-        expect(Array.isArray(data)).toBeTruthty();
+        expect(Array.isArray(data)).toBeTruthy();
         expect(data.length).not.toBeGreaterThan(10);
         expect(data[3]).toBeTruthy();
            
