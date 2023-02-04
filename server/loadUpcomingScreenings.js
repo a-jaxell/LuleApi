7828dd0452bb30868ed7c47cf1ac01502345160f
Rkron91 <robin.kron@lerniastudent.se>
2023-02-04 15:10:56 +0100

    Co-authored-by: dansaa8 <dansaa8@users.noreply.github.com>

--- server/loadUpcomingScreenings.js
+++ server/loadUpcomingScreenings.js
@@ -1,19 +1,21 @@
-import fetch from "node-fetch"
+import fetch from "node-fetch";
 
 export default async function loadUpcomingScreenings(param) {
-
-    const baseUrl =
+  const baseUrl =
     "https://plankton-app-xhkom.ondigitalocean.app/api/screenings?populate=movie&filters[movie]=";
-    
-    const res = await fetch(baseUrl + param)
-    const data = await res.json();
 
-    return data.data.map((elem) => {
-        return {
-            starttime: elem.attributes.start_time,
-            room: elem.attributes.room,
-        };
-    }).filter( elem => elem.starttime > new Date().toISOString())
-    
-    
+  const res = await fetch(baseUrl + param);
+  const data = await res.json();
+
+  return data.data
+    .filter((elem) => {
+      return (elem) => elem.start_time > new Date().toISOString();
+    })
+    .map((elem) => {
+      return {
+        room: elem.attributes.room,
+        date: elem.attributes.start_time.substring(0, 10),
+        time: elem.attributes.start_time.substring(11, 16),
+      };
+    });
 }
