59fd0971d9fa3f1423072861911abc4af795e866
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-02-08 19:49:37 +0100

    Add movie-details container element and renamed for easier understandability

--- static/jsfrontend/getAllScreenings.js
+++ static/jsfrontend/getAllScreenings.js
@@ -12,26 +12,35 @@ const renderScreenings = async () => {
 
         data.forEach(ele => {
             
-            const movie = document.createElement('li');
-            movie.className = "screening-item";
-            movie.classList.add("screening");
-            screeningList.appendChild(movie);
+            const screening = document.createElement('li');
+            screening.className = "screening-item";
+            screening.classList.add("screening");
+            screeningList.appendChild(screening);
 
             const movieImage = document.createElement('img');
             movieImage.classList.add("screening-list-image");
             movieImage.src = `${ele.movie.image.url}`;
-            movie.appendChild(movieImage);
+            screening.appendChild(movieImage);
             
-            const anchor = document.createElement('a');
-            anchor.innerText = ele.movie.title;
-            anchor.href = `/movies/${ele.movie.id}`;
-            anchor.className = "movie-title";
-            movie.appendChild(anchor);
+            const screeningDetails = document.createElement('div');
+            screeningDetails.className = "movie-details";
+            screening.appendChild(screeningDetails);
+
+            const title = document.createElement('a');
+            title.innerText = ele.movie.title;
+            title.href = `/movies/${ele.movie.id}`;
+            title.className = "movie-title";
+            screeningDetails.appendChild(title);
 
             const showTime = document.createElement('a');
             showTime.innerText = ` ${ele.start_time.substring(11,16)} ${ele.start_time.substring(0,10)}`;
-            showTime.className = ".screening-time"
-            movie.appendChild(showTime);
+            showTime.className = "screening-time"
+            screeningDetails.appendChild(showTime);
+
+            const showRoom = document.createElement('p');
+            showRoom.innerText = `${ele.room}`;
+            showRoom.className = "screening-room";
+            screeningDetails.appendChild(showRoom);
 
         }));
         container.append(screeningList);
