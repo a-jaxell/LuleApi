fa295bd7853d5c6344736e8cb3c32f24a9e62f6d
Rasmus Eliasson <ras.muse@hotmail.com>
2023-02-08 17:41:28 +0100

    Moved some rating code to server

--- server/server.js
+++ server/server.js
@@ -36,7 +36,36 @@ app.get("/movies/:id/rating", async (req, res) => {
     `https://plankton-app-xhkom.ondigitalocean.app/api/reviews?filters%5Bmovie%5D=${movieID}`
   );
   const reviewData = await response.json();
-  res.send(reviewData);
+
+  const data = reviewData.data;
+  let allRating = data.map((rating) => {
+    return rating.attributes.rating;
+  });
+
+  //removing value of null if it exist
+  let nullValue = null;
+  allRating = allRating.filter((item) => item !== nullValue);
+
+  //function to get average rating.
+  function averageRating(array) {
+    let sum = 0;
+
+    //round rating to 1 decimal
+    array.forEach((rating) => {
+      sum += rating / allRating.length;
+      sum = Math.round(sum * 10) / 10;
+    });
+
+    return sum;
+  }
+
+  if (allRating.length >= 5) {
+    let roundedRating = averageRating(allRating);
+    res.status(200).send({ body: roundedRating });
+  } else {
+    //här ska funktion med imbd api in
+    return;
+  }
 });
 
 app.post("/movies/:id/review", async (req, res) => {
--- static/jsfrontend/moviePage.js
+++ static/jsfrontend/moviePage.js
@@ -7,7 +7,7 @@ const sendBtn = document.querySelector(".send-btn");
 const idUrl = document.location.toString();
 const movieId = idUrl.slice(29, 30);
 
-sendBtn.addEventListener("click", async function (req, res) {
+sendBtn.addEventListener("click", async function () {
   event.preventDefault();
   if (firstName.value === "") {
     alert("Du har inte fyllt förnamn");
@@ -37,32 +37,7 @@ async function getMovieRating() {
     `http://localhost:5080/movies/${movieId}/rating`
   );
   const dataJson = await response.json();
-  const data = dataJson.data;
-  let allRating = data.map((rating) => {
-    return rating.attributes.rating;
-  });
-
-  //removing value of null if it exist
-  let nullValue = null;
-  allRating = allRating.filter((item) => item !== nullValue);
-
-  if (allRating.length >= 5) {
-    const roundedRating = averageRating(allRating);
-
-    document.querySelector(".movie-rating").append(roundedRating);
-  }
-
-  //function to get average rating.
-  function averageRating(array) {
-    let sum = 0;
-
-    //round rating to 1 decimal
-    array.forEach((rating) => {
-      sum += rating / allRating.length;
-      sum = Math.round(sum * 10) / 10;
-    });
-
-    return sum;
-  }
+  document.querySelector(".movie-rating").append(dataJson.body);
 }
+
 getMovieRating();
