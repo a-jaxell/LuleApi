7ac14345107f97cbfbfba7c9300de7bbd428d763
Rasmus Eliasson <ras.muse@hotmail.com>
2023-02-09 09:57:24 +0100

    change id so works with 2 didget numbers

--- static/jsfrontend/moviePage.js
+++ static/jsfrontend/moviePage.js
@@ -4,81 +4,44 @@ const rating = document.querySelector(".rating");
 const commentField = document.querySelector(".comment-field");
 const sendBtn = document.querySelector(".send-btn");
 
-const idUrl = document.location.toString();
-const movieId = idUrl.slice(29, 30);
+//get id from url
+const idUrl = new URL(document.URL).pathname.split("/");
+const movieId = idUrl.pop() || idUrl.pop();
 
-sendBtn.addEventListener("click", async function () {
-  event.preventDefault();
-  if (firstName.value === "") {
-    alert("Du har inte fyllt förnamn");
-  } else if (lastName.value === "") {
-    alert("Du har inte fyllt efternamn");
-  } else if (commentField.value === "") {
-    alert("Du har inte angivit någon kommentar");
-  } else {
-    await fetch(`http://localhost:5080/movies/${movieId}/review`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-
-      body: JSON.stringify({
-        data: {
-          comment: commentField.value,
-          rating: rating.value,
-          author: firstName.value + " " + lastName.value,
-          verified: true,
-          movie: movieId,
-        },
-      }),
-    });
+if (sendBtn) {
+  async function getMovieRating() {
+    const response = await fetch(
+      `http://localhost:5080/movies/${movieId}/rating`
+    );
+    const dataJson = await response.json();
+    document.querySelector(".movie-rating").append(dataJson.body);
   }
-});
 
-async function getMovieRating() {
-  const response = await fetch(
-    `http://localhost:5080/movies/${movieId}/rating`
-  );
-  const dataJson = await response.json();
-  document.querySelector(".movie-rating").append(dataJson.body);
-}
+  getMovieRating();
 
-getMovieRating();
+  sendBtn.addEventListener("click", async function () {
+    event.preventDefault();
+    if (firstName.value === "") {
+      alert("Du har inte fyllt förnamn");
+    } else if (lastName.value === "") {
+      alert("Du har inte fyllt efternamn");
+    } else if (commentField.value === "") {
+      alert("Du har inte angivit någon kommentar");
+    } else {
+      await fetch(`${movieId}/review`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
 
-document.querySelector(".test-btn").addEventListener("click", () => {
-  event.preventDefault();
-  jwt();
-});
-
-async function jwt() {
-  const firstName = document.querySelector(".hej").value;
-  const lastName = document.querySelector(".då").value;
-  const capitalCityStockholm = document.querySelector(".capital-city").value;
-
-  const credentials = `${firstName}:${lastName}`;
-  const b64 = btoa(credentials);
-
-  const data = { capital: capitalCityStockholm };
-
-  const resToken = await fetch(
-    `http://localhost:5080/movies/${movieId}/sendReview`,
-    {
-      method: "POST",
-
-      headers: {
-        Authorization: "Basic " + b64,
-        "Content-Type": "application/json",
-      },
-
-      body: JSON.stringify({
-        data,
-      }),
+        body: JSON.stringify({
+          data: {
+            comment: commentField.value,
+            rating: rating.value,
+            author: firstName.value + " " + lastName.value,
+            verified: true,
+            movie: movieId,
+          },
+        }),
+      });
     }
-  );
-  const dataToken = await resToken.json();
-  console.log(dataToken);
-
-  const res = await fetch(`http://localhost:5080/movies/${movieId}/protected`, {
-    headers: { Authorization: "Bearer " + dataToken.token },
   });
-  const payload = await res.json();
-  console.log(payload);
 }
