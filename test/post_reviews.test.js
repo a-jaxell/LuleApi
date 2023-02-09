26033a6d27a7f76806d85f040ba2c86748feb86a
liber09 <linda.bergsangel@hotmail.com>
2023-02-09 05:55:17 +0100

    more testcases

--- test/post_reviews.test.js
+++ test/post_reviews.test.js
@@ -3,8 +3,8 @@ import { describe, expect, test } from "@jest/globals";
 import { Router } from "express";
 
 describe("Test review validation", () => {
-	test("Author should be at list three characters", async () => {
-        //Get reviews for movie with ID=2 and page=1
+	test("Author should be at least three characters", async () => {
+        
 		const result = router.post("/test",(req,res) =>{
             const review = {
                 author: kalle,
@@ -14,17 +14,52 @@ describe("Test review validation", () => {
             
             
         })
-        //Check so that we don't get more than 5 reviews in response
+        
 		expect();
 	});
 
-	test("Get different pages", async () => {
-        //Get reviews for movie with ID=2 and page=1
-		const result1 = await loadReviews(2,1);
-        //Get reviews for movie with ID=2 and page=2
-		const result2 = await loadReviews(2,2);
+	test("comment should be at least three characters", async () => {
+        
+		const result = router.post("/test",(req,res) =>{
+            const review = {
+                author: kalle,
+                rating: 3,
+                comment: "medelmåttigt"
+            }
+            
+            
+        })
 
         //Check so thet first review of first page is not the same as firsrt review on second page
 		expect(result1.reviews.data[0].id).not.toStrictEqual(result2.reviews.data[0].id);
+	});
+
+    test("rating should be integer between 0 and 5", async () => {
+        
+		const result = router.post("/test",(req,res) =>{
+            const review = {
+                author: kalle,
+                rating: 3,
+                comment: "medelmåttigt"
+            }
+            
+            
+        })
+        
+		expect();
+	});
+    test("rating should be integer between 0 and 5_fails", async () => {
+        
+		const result = router.post("/test",(req,res) =>{
+            const review = {
+                author: kalle,
+                rating: 6,
+                comment: "medelmåttigt"
+            }
+            
+            
+        })
+        
+		expect();
 	});
 });
