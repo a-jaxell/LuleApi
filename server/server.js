1d465e8a73306c52cf26445d31a122d49acc8a4c
liber09 <linda.bergsangel@hotmail.com>
2023-02-09 05:51:30 +0100

    small changes and added some comments

--- server/server.js
+++ server/server.js
@@ -52,17 +52,20 @@ app.post("/movies/:id/review",
   body('rating').isInt({min:0, max:5
   }),
   async (req,res) => {
+    //Are there any validation errors
     const errors = validationResult(req);
 
+    //If validation errors, sen status 400 and errors array
     if (!errors.isEmpty()){
       return res.status(400).json({
         success: false,
         errors: errors.array()
       });
     }
+    //If no errors send 200 success
     res.status(200).json({
       success: true,
-      message: 'post successful'
+      message: 'Review posted successfully'
     })
   });
 
