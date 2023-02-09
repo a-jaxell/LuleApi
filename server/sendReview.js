bbbb16abf5857c8e319976d8091faef0acee439b
Rasmus Eliasson <ras.muse@hotmail.com>
2023-02-09 16:00:04 +0100

    some test

--- server/sendReview.js
+++ server/sendReview.js
@@ -11,10 +11,34 @@ sendReviewServer.post("/movies/:id/review", async (req, res) => {
   });
 });
 
-export async function getReview() {
-  const res = await fetch(
-    "https://plankton-app-xhkom.ondigitalocean.app/api/reviews"
-  );
-  const dataReview = await res.json();
-  return dataReview.data;
+export const api = {
+  loadReview: async function () {
+    const res = await fetch(
+      "https://plankton-app-xhkom.ondigitalocean.app/api/reviews"
+    );
+    const dataReview = await res.json();
+    return dataReview;
+  },
+};
+
+export default async function sortReview(mock) {
+  const payload = await api.loadReview();
+
+  const result = payload.data
+    .map((item) => ({
+      id: item.id,
+      ...item.attributes,
+    }))
+    .filter((review) => {
+      return review.rating >= 3;
+    })
+    .filter((review) => {
+      const createdAtDate = new Date(review.createdAt);
+      const now = new Date();
+      const diff = now - createdAtDate;
+      const sixtyDays = 60 * 24 * 60 * 60 * 1000;
+      return diff < sixtyDays;
+    });
+
+  return result;
 }
