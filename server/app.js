b44eee9472180d7e6c1d4c8287568319e2b100fe
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-02-10 14:27:08 +0100

    changed port to 8080 to work with digital ocean

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 import app from "./server.js";
 
-const PORT = 5080 || process.env.PORT;
+const PORT = 8080 || process.env.PORT;
  
 app.listen(PORT, () => {
     console.log("Server is listening on port: " + PORT);
