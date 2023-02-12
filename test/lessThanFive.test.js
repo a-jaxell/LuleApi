8a1f0e35066eefbec96961476f51ce48d19d57e0
burrozd <95920902+burrozd@users.noreply.github.com>
2023-02-12 01:01:40 +0100

    omdb Test

--- test/lessThanFive.test.js
+++ test/lessThanFive.test.js
@@ -1,110 +1,41 @@
-import { displayRating, roundRating, loadOMDB } from "./rating.js";
-import fetch from "node-fetch";
-import { mockApiAdapter } from "./mockApiAdapter.js";
-
-jest.mock("node-fetch", () => jest.fn());
-
-/* const mockApiAdapter = {
-    loadData: async() => {
-        return {
-            data: [{
-                    attributes: {
-                        rating: 3,
-                    },
-                },
-                {
-                    attributes: {
-                        rating: 4,
-                    },
-                },
-                {
-                    attributes: {
-                        rating: 5,
-                    },
-                },
-                {
-                    attributes: {
-                        rating: 2,
-                    },
-                },
-                {
-                    attributes: {
-                        rating: 1,
-                    },
-                },
-                {
-                    attributes: {
-                        rating: 3,
-                    },
-                },
-                {
-                    attributes: {
-                        rating: 4,
-                    },
-                },
-                {
-                    attributes: {
-                        rating: 2,
-                    },
-                },
-                {
-                    attributes: {
-                        rating: 5,
-                    },
-                },
-                {
-                    attributes: {
-                        rating: 3,
-                    },
-                },
-            ],
-        };
-    },
-};
+import { describe, expect, test, jest } from "@jest/globals";
+/* import { loadOMDB as omdbTest } from "../server/rating.js";
  */
-describe("displayRating", () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+const movie = {
+    id: 123,
+    title: "The Matrix",
+    reviews: [
+        { rating: 4 },
+        { rating: 5 },
+        { rating: 3 },
+        { rating: 4 },
+        { rating: 5 },
+    ],
+};
 
-    describe("when there are 5 or more revienpmws for a movie", () => {
-        it("calculates the average rating from these reviews", async() => {
-            fetch.mockImplementation(() =>
-                Promise.resolve({
-                    json: () => Promise.resolve(mockApiAdapter.loadData()),
-                })
-            );
-            const req = { params: { id: 1 } };
-            const res = {
-                status: jest.fn().mockReturnThis(),
-                send: jest.fn(),
-            };
+const omdbTest = jest.fn().mockResolvedValue(4.5);
 
-            await displayRating.get("/movies/:id/rating", req, res);
-            const averageRating = roundRating([3, 4, 5, 2, 1]);
-
-            expect(res.status).toHaveBeenCalledWith(200);
-            expect(res.send).toHaveBeenCalledWith({ body: averageRating });
-        });
-    });
-
-    describe("when there are fewer than 5 reviews for a movie", () => {
-        it("retrieves the average rating from IMDB", async() => {
-            fetch.mockImplementation(() =>
-                Promise.resolve({
-                    json: () => Promise.resolve({ imdbRating: "9.0" }),
-                })
-            );
-            const req = { params: { id: 1 } };
-            const res = {
-                status: jest.fn().mockReturnThis(),
-                send: jest.fn(),
-            };
-
-            await displayRating.get("/movies/:id/rating", req, res);
-
-            expect(res.status).toHaveBeenCalledWith(200);
-            expect(res.send).toHaveBeenCalledWith({ body: "9.0" });
-        });
-    });
+test("calculates average rating from reviews if there are 5 or more", async() => {
+    const average = calculateAverageRating(movie);
+    expect(average).toBe(4.2);
+    expect(omdbTest).not.toHaveBeenCalled();
 });
+
+test("retrieves OMDB rating if there are fewer than 5 reviews", async() => {
+    movie.reviews.pop();
+    const average = calculateAverageRating(movie);
+    expect(average).toBe(4.5);
+    expect(omdbTest).toHaveBeenCalledWith(movie.id);
+});
+
+function calculateAverageRating(movie) {
+    if (movie.reviews.length >= 5) {
+        let total = 0;
+        movie.reviews.forEach((review) => {
+            total += review.rating;
+        });
+        return total / movie.reviews.length;
+    } else {
+        return omdbTest(movie.id);
+    }
+}
