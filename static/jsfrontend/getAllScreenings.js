c5484d6685a15c81984e25640c45fcf2b5cd91ec
burrozd <95920902+burrozd@users.noreply.github.com>
2023-03-21 11:37:48 +0100

    alt text for images

--- static/jsfrontend/getAllScreenings.js
+++ static/jsfrontend/getAllScreenings.js
@@ -1,48 +1,48 @@
-
 const container = document.querySelector(".screening-container");
 
-const renderScreenings = async () => {
-        
-        const res = await fetch("/screenings");
-        
-        const data = await res.json();
-        
-        const screeningList = document.createElement('ul');
-        screeningList.className = "screening-list-main-page";
+const renderScreenings = async() => {
+    const res = await fetch("/screenings");
 
-        data.forEach(ele => {
-            
-            const screening = document.createElement('li');
-            screening.className = "screening-item";
-            screening.classList.add("screening");
-            screeningList.appendChild(screening);
+    const data = await res.json();
 
-            const movieImage = document.createElement('img');
-            movieImage.classList.add("screening-list-image");
-            movieImage.src = `${ele.movie.image.url}`;
-            screening.appendChild(movieImage);
-            
-            const screeningDetails = document.createElement('div');
-            screeningDetails.className = "movie-details";
-            screening.appendChild(screeningDetails);
+    const screeningList = document.createElement("ul");
+    screeningList.className = "screening-list-main-page";
 
-            const title = document.createElement('a');
-            title.innerText = ele.movie.title;
-            title.href = `/movies/${ele.movie.id}`;
-            title.className = "movie-title";
-            screeningDetails.appendChild(title);
+    data.forEach((ele) => {
+        const screening = document.createElement("li");
+        screening.className = "screening-item";
+        screening.classList.add("screening");
+        screeningList.appendChild(screening);
 
-            const showTime = document.createElement('a');
-            showTime.innerText = ` ${ele.start_time.substring(11,16)} ${ele.start_time.substring(0,10)}`;
-            showTime.className = "screening-time"
-            screeningDetails.appendChild(showTime);
+        const movieImage = document.createElement("img");
+        movieImage.classList.add("screening-list-image");
+        movieImage.src = `${ele.movie.image.url}`;
+        movieImage.setAttribute("alt", "`${/movies/${ele.movie.id}}`");
+        screening.appendChild(movieImage);
 
-            const showRoom = document.createElement('p');
-            showRoom.innerText = `${ele.room}`;
-            showRoom.className = "screening-room";
-            screeningDetails.appendChild(showRoom);
+        const screeningDetails = document.createElement("div");
+        screeningDetails.className = "movie-details";
+        screening.appendChild(screeningDetails);
 
-        });
-        container.append(screeningList);
-}
+        const title = document.createElement("a");
+        title.innerText = ele.movie.title;
+        title.href = `/movies/${ele.movie.id}`;
+        title.className = "movie-title";
+        screeningDetails.appendChild(title);
+
+        const showTime = document.createElement("a");
+        showTime.innerText = ` ${ele.start_time.substring(
+      11,
+      16
+    )} ${ele.start_time.substring(0, 10)}`;
+        showTime.className = "screening-time";
+        screeningDetails.appendChild(showTime);
+
+        const showRoom = document.createElement("p");
+        showRoom.innerText = `${ele.room}`;
+        showRoom.className = "screening-room";
+        screeningDetails.appendChild(showRoom);
+    });
+    container.append(screeningList);
+};
 renderScreenings();
