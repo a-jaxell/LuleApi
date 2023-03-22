862faeccc5e24c52923372af722fab286cdc3043
burrozd <95920902+burrozd@users.noreply.github.com>
2023-03-22 19:57:20 +0100

    cypress test

---
+++ cypress/e2e/running.spec.js
@@ -0,0 +1,6 @@
+/* describe("Check if localhost is running", () => {
+    it("Should be able to access localhost", () => {
+        cy.visit("http://localhost:8080");
+        cy.contains("Kommande visningar");
+    });
+}); */
